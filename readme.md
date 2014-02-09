bfe3880a8cd282a6250dab5ca728f966f0b6b243
Techwolf <dring@g33kworld.net>
2014-02-09 15:04:23 -0800

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,5 @@
+FRC Scout
+---------
+A simple scouting web application for use in FRC robotics competitions.
+
+It currently contains a form for the 2014 FRC season, but can easily be updated for use in future competitions.
---
+++ scout.php
@@ -0,0 +1,178 @@
+<?php
+	if($_GET["action"] == "record") {
+		date_default_timezone_set("America/Los Angeles");
+		$_POST["timestamp"] = date("Y-m-d h:i:s A");
+		$datafile = fopen("scouting.csv", "a");
+		fputcsv($datafile, $_POST);
+		fclose($datafile);
+		header("Location: ?action=menu&teamNum=$_POST[teamNum]&roundNum=$_POST[roundNum]");
+		die();
+	}
+?>
+
+<html>
+
+<head>
+	<title>Team 691 Scouting</title>
+</head>
+
+<body bgcolor="#222222" style="color:#DDDDDD;">
+
+<?php
+
+$locale = array(
+	array("Team Number", "teamNum"),
+	array("Round Number", "roundNum"),
+	array("Alliance Color", "allianceColor"),
+	array("Blue Score", "blueScore"),
+	array("Red Score", "redScore"),
+	array("Autonomous", "autonomous"),
+	array("Teleop", "teleop"),
+	array("Drive", "drive"),
+	array("Omnidirectional Drive", "driveOmni"),
+	array("Drive Speed", "driveSpeed"),
+	array("Drive Power", "drivePower"),
+	array("Offense", "offense"),
+	array("Defense", "defense"),
+	array("Truss", "truss"),
+	array("Pass", "pass"),
+	array("Catch", "catch"),
+	array("Shoot", "shoot"),
+	array("Shoot Success (%)", "shootSuccess"),
+	array("Ground Pickup", "groundPickup"),
+	array("Comments", "comments"),
+	array("Submitter Team Number", "submitter"),
+	array("Timestamp","timestamp")
+);
+
+$addform = <<<EOT
+<form action="?action=record" method="post">
+{$locale[0][0]}: <input name="{$locale[0][1]}" type="number" min="0" step="1" required><br>
+{$locale[1][0]}: <input name="{$locale[1][1]}" type="number" min="0" step="1" required><br>
+{$locale[2][0]}:
+<select name="{$locale[2][1]}" required>
+	<option selected disabled hidden></option>
+	<option value="blue">Blue</option>
+	<option value="red">Red</option>
+</select><br>
+{$locale[3][0]}: <input name="{$locale[3][1]}" type="number" min="0" step="1" required><br>
+{$locale[4][0]}: <input name="{$locale[4][1]}" type="number" min="0" step="1" required><br>
+{$locale[5][0]}: <input name="{$locale[5][1]}" type="radio" value="true" required>Yes<input name="{$locale[5][1]}" type="radio" value="false" required>No<br>
+{$locale[6][0]}: <input name="{$locale[6][1]}" type="radio" value="true" required>Yes<input name="{$locale[6][1]}" type="radio" value="false" required>No<br>
+{$locale[7][0]}: <input name="{$locale[7][1]}" type="radio" value="true" required>Yes<input name="{$locale[7][1]}" type="radio" value="false" required>No<br>
+{$locale[8][0]}: <input name="{$locale[8][1]}" type="radio" value="true" required>Yes<input name="{$locale[8][1]}" type="radio" value="false" required>No<br>
+{$locale[9][0]}:
+<select name="{$locale[9][1]}" required>
+	<option selected disabled hidden></option>
+	<option value="none">None</option>
+	<option value="slow">Slow</option>
+	<option value="medium">Medium</option>
+	<option value="fast">Fast</option>
+</select><br>
+{$locale[10][0]}:
+<select name="{$locale[10][1]}" required>
+	<option selected disabled hidden></option>
+	<option value="none">None</option>
+	<option value="low">Low</option>
+	<option value="medium">Medium</option>
+	<option value="high">High</option>
+</select><br>
+{$locale[11][0]}: <input name="{$locale[11][1]}" type="radio" value="true" required>Yes<input name="{$locale[11][1]}" type="radio" value="false" required>No<br>
+{$locale[12][0]}: <input name="{$locale[12][1]}" type="radio" value="true" required>Yes<input name="{$locale[12][1]}" type="radio" value="false" required>No<br>
+{$locale[13][0]}: <input name="{$locale[13][1]}" type="radio" value="true" required>Yes<input name="{$locale[13][1]}" type="radio" value="false" required>No<br>
+{$locale[14][0]}:
+<select name="{$locale[14][1]}" required>
+	<option selected disabled hidden></option>
+	<option value="none">None</option>
+	<option value="toss">Toss</option>
+	<option value="push">Push</option>
+	<option value="both">Both</option>
+</select><br>
+{$locale[15][0]}: <input name="{$locale[15][1]}" type="radio" value="true" required>Yes<input name="{$locale[15][1]}" type="radio" value="false" required>No<br>
+{$locale[16][0]}:
+<select name="{$locale[16][1]}" required>
+	<option selected disabled hidden></option>
+	<option value="none">None</option>
+	<option value="low">Low</option>
+	<option value="high">High</option>
+	<option value="both">Both</option>
+</select><br>
+{$locale[17][0]}: <input name="{$locale[17][1]}" type="number" min="0" step="1" required><br>
+{$locale[18][0]}: <input name="{$locale[18][1]}" type="radio" value="true" required>Yes<input name="{$locale[18][1]}" type="radio" value="false" required>No<br>
+{$locale[19][0]}: <input name="{$locale[19][1]}" type="text"><br>
+{$locale[20][0]}: <input name="{$locale[20][1]}" type="number" min="0" step="1" required><br>
+<input type="submit">
+</form>
+EOT;
+
+	if($_GET["action"] == "add") {
+		echo $addform;
+	} else if($_GET["action"] == "view") {
+		$data = array();
+		$datafile = fopen("scouting.csv", "r");
+		while(!feof($datafile)) {
+			$data[] = fgetcsv($datafile);
+		}
+		fclose($datafile);
+		$result = array();
+		if(isset($_GET["teamNum"])) {
+			if(isset($_GET["roundNum"])) {
+				//Return row
+				$i = 0;
+				foreach($data as $record) {
+					if($record[0] == $_GET["teamNum"] && $record[1] == $_GET["roundNum"]) {
+						if($i > 0) {
+							echo "<p>\nIf you're seeing this, someone screwed up. I hate users. Go away.\n</p>\n";
+						}
+						$i++;
+						echo "Team: {$_GET[teamNum]}<br>\n";
+						echo "Round: {$_GET[roundNum]}<br>\n";
+						foreach($record as $key => $value) {
+							echo $locale[$key][0].": ".$value."<br>\n";
+						}
+					}
+				}
+			} else {
+				//Return rounds
+				foreach($data as $record) {
+					if($record[0] == $_GET["teamNum"]) {
+						$result[] = $record[1];
+					}
+				}
+				echo "Team: {$_GET[teamNum]}<br>\n";
+				echo "Select a round:<br>\n";
+				foreach(array_unique($result) as $round) {
+					echo "<a href=\"?action=view&teamNum={$_GET[teamNum]}&roundNum=$round\">Round $round</a><br>\n";
+				}
+			}
+		} else {
+			//Return teams
+			foreach($data as $record) {
+				if($record[0] != null) {
+					$result[] = $record[0];
+				}
+			}
+			echo "Select a team:<br>\n";
+			foreach(array_unique($result) as $team) {
+				echo "<a href=\"?action=view&teamNum=$team\">Team $team</a><br>\n";
+			}
+		}
+	} else if($_GET["action"] == "get") {
+		echo file_get_contents("scouting.csv");
+	} else if($_GET["action"] == "menu") {
+		echo "<h1>\n";
+		echo "<a href=\"?action=add\">Add Record</a><br>\n";
+		echo "<a href=\"?action=view\">View Records</a><br>\n";
+		echo "<strong>Record added for team $_GET[teamNum] in round $_GET[roundNum]!</strong>";
+		echo "</h1>\n";
+	} else {
+		echo "<h1>\n";
+		echo "<a href=\"?action=add\">Add Record</a><br>\n";
+		echo "<a href=\"?action=view\">View Records</a><br>\n";
+		echo "</h1>\n";
+	}
+?>
+
+</body>
+
+</html>
